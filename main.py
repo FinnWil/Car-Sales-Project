30251851b116234f205d5d4d881ae7f80f7e28fb
FinnWil <92762731+FinnWil@users.noreply.github.com>
2022-08-31 21:14:57 +1200

    update

--- main.py
+++ main.py
@@ -94,25 +94,3 @@ ann_viz(model, view=True, filename="network.gv", title="ANN")
 graph_file = graphviz.Source.from_file('network.gv')
 graph_file
 
-sg.theme('BluePurple')
-
-layout = [[sg.Text('Your typed characters appear here:'),
-           sg.Text(size=(15,1), key='-OUTPUT-')],
-          [sg.Input(key='-IN-')],
-          [sg.Button('Display'), sg.Button('Exit')]]
-
-window = sg.Window('Introduction', layout)
-
-while True:
-    event, values = window.read()
-    print(event, values)
-
-    if event in  (None, 'Exit'):
-        break
-
-    if event == 'Display':
-        # Update the "output" text element
-        # to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
-
-window.close()
